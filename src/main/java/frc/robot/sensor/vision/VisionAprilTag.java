592ecaf2cb40173de2aa01d6aff19015dc7a7610
Gloria Jin <gjin7909@gmail.com>
2024-03-02 12:39:51 -0800

    photon vision

--- src/main/java/frc/robot/sensor/vision/VisionAprilTag.java
+++ src/main/java/frc/robot/sensor/vision/VisionAprilTag.java
@@ -1,16 +1,98 @@
 package frc.robot.sensor.vision;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
+import static frc.robot.Constants.aprilTagFieldLayout;
+import frc.robot.sensor.vision.AprilTagConstants.*;
+import static frc.robot.sensor.vision.AprilTagConstants.normalMultiTagStdDev;
+import static frc.robot.sensor.vision.AprilTagConstants.highResMultiTagStdDev;
+import static frc.robot.sensor.vision.AprilTagConstants.normalSingleTagStdDev;
+import static frc.robot.sensor.vision.AprilTagConstants.highResSingleTagStdDev;
+
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
-import frc.robot.Constants;
-import frc.robot.sensor.pose.Odometry;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import java.util.List;
+import org.littletonrobotics.junction.AutoLog;
+import org.photonvision.EstimatedRobotPose;
+
 
 
 import java.util.Optional;
 
 
 
-public class VisionAprilTag {
+public interface VisionAprilTag {
+
+    @AutoLog
+    public static class AprilTagVisionIOInputs{
+        public Pose3d[] visionPoses = 
+            List.of(new Pose3d(), new Pose3d(), new Pose3d()).toArray(new Pose3d[0]); 
+        public double[] timestamps = new double[3];
+        public double[] visionStdDevs = new double[9];
+    }
+
+    public default void updateInputs(AprilTagVisionIOInputs inputs){}
+
+    public default void updatePose(Pose2d pose){}
+
+
+    default Matrix<N3, N1> getEstimationStdDevs(
+        
     
+        EstimatedRobotPose estimatedPose, CameraResolution resolution){
+            var estStdDevs = 
+                switch(resolution){
+                    case HIGH_RES -> highResSingleTagStdDev; 
+                    case NORMAL -> normalSingleTagStdDev; 
+                }; 
+                    
+        var targets = estimatedPose.targetsUsed;
+        int numTags = 0;
+        double avgDist = 0;
+        for (var tgt : targets) {
+        var tagPose = aprilTagFieldLayout.getTagPose(tgt.getFiducialId());
+        if (tagPose.isEmpty()) continue;
+        numTags++;
+        avgDist +=
+            tagPose
+                .get()
+                .toPose2d()
+                .minus(estimatedPose.estimatedPose.toPose2d())
+                .getTranslation()
+                .getNorm();
+        }
+        if (numTags == 0) return estStdDevs;
+        avgDist /= numTags;
+
+
+
+        // Decrease std devs if multiple targets are visible
+        if (numTags > 1)
+        estStdDevs =
+            switch (resolution) {
+                case HIGH_RES -> highResMultiTagStdDev;
+                case NORMAL -> normalMultiTagStdDev;
+            };
+
+
+
+        // Increase std devs based on (average) distance
+        if (numTags == 1
+            && avgDist
+                > switch (resolution) {
+                case HIGH_RES -> 6;
+                case NORMAL -> 4;
+                }) {
+        estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+        } else {
+        estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
+        }
+
+        return estStdDevs;
+  }
 }
+        
+    
